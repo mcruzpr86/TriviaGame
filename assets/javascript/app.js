4e86985664651ef4cad4e4ad1101bd6abce2567d
mcruz <mcruzpr86@gmail.com>
2020-04-28 17:59:54 -0400

    javescript functionality started

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,63 +1,65 @@
 //JavaScript
     
    
-    var questions = [];
-    var answer = '';
-    var guessed = [];
-    var maxWrong = 6;
-    var mistakes = 0;
-    let wordStatus = null;
+    var correct;
+    let incorrect;
+    let timer;
+
+    let questions = [
+        {
+            Question: "Whos is the leader of the group",
+            Answers: ['Leonardo', 'Master Splinter', 'April Oneil', 'Michaelangelo'],
+            correctAnswer: 'Master Splinter',
+        },
+        {
+            Question: "The happy go lucky dude that loves pizza is",
+            Answers: ['Leonardo', 'Master Splinter', 'April Oneil', 'Michaelangelo'],
+            correctAnswer: 'Master Splinter',
+        },
+        {
+            Question: "Whos is the leader of the group",
+            Answers: ['Leonardo', 'Master Splinter', 'April Oneil', 'Michaelangelo'],
+            correctAnswer: 'Master Splinter',
+        },
+        {
+            Question: "Whos is the leader of the group",
+            Answers: ['Leonardo', 'Master Splinter', 'April Oneil', 'Michaelangelo'],
+            correctAnswer: 'Master Splinter',
+        },
+    ]
 
 
-    //console.log
+//start button
 
-    for (var i = 0; i < characters.length; i++) {
-      console.log(characters[i]);
-   }
+$(document).on('click', '#start', function(){
+    start();
+})
 
+function start() {
+    correct = 0;
+    incorrect = 0;
+    timer = 120;
 
-    // Listener
-    document.onkeydown = function (event) {
-      console.log(event.key)
-    }
-
-
-//press start to begin. 
-
-  document.onkeyup = function(event) {
-    var startGame = String.fromCharCode(event.keycode).toLowerCase();
-    alert('COWABUNGA DUDES! The Game Has Begun');
-  
-  }
-
-  // Random word generator
+    setInterval(() => {
+       timer -- 
+       if (timer == 0) {
+          gameDone()
+          console.log('game finished')  
+       }
+    $('#timer').html(timer)
+    }, 1000);
     
-  function characterAnswers() {
-    var randomWord = characters[Math.floor (Math.random() * characters.length)];
-    console.log (randomWord)
-
-  }
-  
-
-
-function updateMistakes() {
-  document.getElementById('mistakes').innerHTML = mistakes;
+    function gameDone() {
+        console.log('end game')
+    }
+    $('#container').prepend(
+        '<h2>Time Remaining</h2><span id= "timer">120</span>'
+    )
+    $('#start').remove()
 }
 
-//reset the game
-function reset() {
-  let text = document.getElementById('reset');
-  text.innerText = 'Restart Game'
-  counter = 0;
-  
-  characterAnswers();
-  guessedWord();
-  updateMistakes();
-  generateButtons();
+//hide time and questions prior to game starting
 
-}
+ 
 
-characterAnswers();
-generateButtons();
-guessedWord();
     
