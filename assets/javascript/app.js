2d0af7dc0669022943bb2b48c823b2dc0ae52edd
mcruz <mcruzpr86@gmail.com>
2020-05-01 22:53:36 -0400

    update

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,104 +1,180 @@
+
 //$(document).ready(function(){
 //TriviaGame
     
-    let correct;
-    let incorrect;
-    let timer;
-    let intervalID;
-    let unanswerCount;
-    let indexQandA = 0; //index to load a different question each round without the game reset or screen refresh
-    let answered = false; //variable to stop the timer if user has clicked an answer
-    let correctAnswers= 0;
-    let incorrectAnswers= 0;
-    let triviaGame = [
-        {
-           Question: "Whos is the leader of the group",
-           Answer: ['Leonardo', 'Master Splinter', 'April Oneil', 'Michaelangelo'],
-           correct: 'Master Splinter',
-        },
-        {
-            Question: "The happy go lucky dude that loves pizza is",
-            Answer: ['Leonardo', 'Master Splinter', 'Donatello', 'Michaelangelo'],
-            answer: 'Michaelangelo',
-        },
-        {
-            Question: "Who is the turtles worst enemy",
-            Answer: ['Foot Soldier Clan', 'Shredder', 'April Oneil', 'Raphael'],
-            correct: 'Shredder',
-        },
-        {
-            Question: "What transformed the turtles into Teenage Muntant Ninja Turtles",
-            Answer: ['Ooze', 'Microwaved Pizza', 'Slime', 'radiation'],
-            correct: 'Ooze',
-        },
-    ]
+let correct;
+let incorrect;
+let timer;
+let intervalID;
+let indexQandA = 0; //index to load a different question each round without the game reset or screen refresh
+let answered = false; //variable to stop the timer if user has clicked an answer
+let correctAnswers= 0;
+let incorrectAnswers= 0;
+
+//let output = [];
+let questions = [
+    {
+       Questions: "Whos is the leader of the group",
+       Answers: ['Leonardo', 'Master Splinter', 'April Oneil', 'Michaelangelo'],
+       correct: 'Master Splinter',
+    },
+    {
+        Questions: "The happy go lucky dude that loves pizza is",
+        Answer: ['Leonardo', 'Master Splinter', 'Donatello', 'Michaelangelo'],
+        correct: 'Michaelangelo',
+    },
+    {
+        Questions: "Who is the turtles worst enemy",
+        Answer: ['Foot Soldier Clan', 'Shredder', 'April Oneil', 'Raphael'],
+        correct: 'Shredder',
+    },
+    {
+        Questions: "What transformed the turtles into Teenage Muntant Ninja Turtles",
+        Answer: ['Ooze', 'Microwaved Pizza', 'Slime', 'radiation'],
+        correct: 'Ooze',
+    },
+]
+
+var quizContainer = document.getElementById('quiz-area');
+var resultsContainer = document.getElementById('results');
+var submitButton = document.getElementById('submit');
+
+generateQuiz(questions, quizContainer, resultsContainer, submitButton);
 
 
+function generateQuiz(questions, quizContainer, resultsContainer, submitButton){
 //start button
 
 $(document).on('click', '#start', function(){
-    start();
+start();
 })
 
 function start() {
-    correct = 0;
-    incorrect = 0;
-    timer = 5;
-    correctAnswers = 0;
-    incorrectAnswers = 0;
-    unanswerCount = 0;
-    setInterval(timer);
-  
-    //loadQandA();
+correct = 0;
+incorrect = 0;
+timer = 5;
+questions;
+correctAnswers = 0;
+incorrectAnswers = 0;
+unanswerCount = 0;
+setInterval(timer);
 
-    setInterval(() => {
-       timer -- 
-       if (timer === 0) {
-          unanswerCount++;
-          gameDone()
-          stop()
-          clearInterval(timer);
-          
-          //console.log('game finished')  
-       }
-    $('#timer').html(timer)
-    }, 1000);
-    
-    
-    function gameDone() {
-        
-        clearInterval(setInterval);
-        //console.log('end game')
-    }
+//loadQandA();
 
-      //stop timer if reach 0
+setInterval(() => {
+   timer -- 
+   if (timer === 0) {
+      unanswerCount++;
+      gameDone()
+      stop()
+      clearInterval(timer);
       
-    $('#container').prepend(
-        '<h4>Time Remaining</h4><span id= "timer">120</span>'
-        )
+      //console.log('game finished')  
+   }
+$('#timer').html(timer)
+}, 1000);
 
-    $('#questions').prepend(
-      '<h4> Questions: </h4><span> </span>'
+
+function gameDone() {
+    
+    clearInterval(setInterval);
+    //console.log('end game')
+}
+
+  //stop timer if reach 0
+  
+$('#container').prepend(
+    '<h4>Time Remaining</h4><span id= "timer">120</span>'
     )
 
-    $('#start').remove()
+$('#questions').prepend(
+  '<h4> Questions: </h4><span> </span>'
+)
 
-    $('#previous').click(function(){
-     
-     })
+$('#start').remove()
 
-
-
-    //time stop
-    function stop() {
-     running = false;
-      clearInterval(start);
-    }
-    
-clearInterval(start);
-stop();
+$('#previous').click(function(){
  
 
-//Loading the questions
-  }
-//}) end of document ready
+
+function showQuestions(questions, quizContainer){
+        // we'll need a place to store the output and the answer choices
+        var output = [];
+        var answers;
+
+        // for each question...
+        for(var i=0; i<questions.length; i++){
+            
+            // first reset the list of answers
+            answers = [];
+
+            // for each available answer...
+            for(letter in questions[i].answers){
+
+                // ...add an html radio button
+                answers.push(
+                    '<label>'
+                        + '<input type="radio" name="question'+ i +'" value="'+letter+'">'
+                        + letter + ': '
+                        + questions[i].answers[letter]
+                    + '</label>'
+                );
+            }
+
+            // add this question and its answers to the output
+            output.push(
+                '<div class="quiz-area">' + questions[i].question + '</div>'
+                + '<div class="answers">' + answers.join('') + '</div>'
+            );
+        }
+
+        // finally combine our output list into one string of html and put it on the page
+        quizContainer.innerHTML = output.join('');
+    }
+
+
+    function showResults(questions, quizContainer, resultsContainer){
+        
+        // gather answer containers from our quiz
+        var answerContainers = quizContainer.querySelectorAll('.answers');
+        
+        // keep track of user's answers
+        var userAnswer = '';
+        var numCorrect = 0;
+        
+        // for each question...
+        for(var i=0; i<questions.length; i++){
+
+            // find selected answer
+            userAnswer = (answerContainers[i].querySelector('input[name=question'+i+']:checked')||{}).value;
+            
+            // if answer is correct
+            if(userAnswer===questions[i].correctAnswer){
+                // add to the number of correct answers
+                numCorrect++;
+                
+                // color the answers green
+                answerContainers[i].style.color = 'lightgreen';
+            }
+            // if answer is wrong or blank
+            else{
+                // color the answers red
+                answerContainers[i].style.color = 'red';
+            }
+        }
+
+        // show number of correct answers out of total
+        resultsContainer.innerHTML = numCorrect + ' out of ' + questions.length;
+    }
+
+    // show questions right away
+    showQuestions(questions, quizContainer);
+    
+    // on submit, show results
+    submitButton.onclick = function(){
+        showResults(questions, quizContainer, resultsContainer);
+    }
+
+})
+}
+}
